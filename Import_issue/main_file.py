8c7751fb0bf8f86e282bc82a0ccc3c3facf9337e
nitesh bawane <nitesh.bawane1@gmail.com>
2017-05-18 13:00:06 +0530

    way to delete cached module added

--- Import_issue/main_file.py
+++ Import_issue/main_file.py
@@ -2,8 +2,17 @@
 .pyc file is also executable file so whether it imports py or pyc
 code works fine
 """
-
+import sys
 print "main_file started"
 importFile = "sub_file"
 obj = __import__(importFile)
 obj.getdata()
+del sys.modules['sub_file']#this is to remove the subfile module which was chached in sys.module
+# print i
+# del i['sub_file']
+# # print type(i)
+# # for t in i:
+# # 	print t
+i = sys.modules
+for t in i:
+	print t
