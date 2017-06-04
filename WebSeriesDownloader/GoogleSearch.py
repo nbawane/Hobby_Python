57f3febb8e417bcdb1a02a69551b7d3567270607
nitesh bawane <nitesh.bawane1@gmail.com>
2017-06-04 21:56:55 +0530

    GoogleSearch modified to crawl through webpage

--- WebSeriesDownloader/GoogleSearch.py
+++ WebSeriesDownloader/GoogleSearch.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as bs
 import urllib2
 import Hobby_codes.logger.logger as log
 import re
+import os
 
 class google:
 	def __init__(self):
@@ -13,7 +14,7 @@ class google:
 		self.hrefs = []
 		self.dl_list = []
 		self.log = log.log(r'C:\Results\webcrawler')
-
+		self.sitemap	= {}
 	def search(self,series_name):
 		'''
 		gives the search page of google
@@ -25,15 +26,22 @@ class google:
 		google_search_url = 'http://www.google.co.in/search?q='
 		full_search_url = google_search_url+search_query
 		# print full_search_url
-		return self.send_search_request(full_search_url)
-
+		self.send_search_request(full_search_url)
+		return self.get_dls()
 	def send_search_request(self,link):
+		'''
+		makes the soup
+		use other functions to decode soup
+		:param link:
+		:return:
+		'''
 		hdr = {'User-Agent': 'Mozilla/53.0.03'}
 		req = urllib2.Request(link,headers=hdr)
 		webobj = urllib2.urlopen(req)
 		self.soup = bs(webobj,'lxml')
-		#self.log.Info(self.soup)
-		return self.get_dls()
+		# self.log.Info(self.soup)
+		# self.log.Info("$"*100)
+		#return self.get_dls()
 
 	def get_dls(self):
 		'''
@@ -57,22 +65,32 @@ class google:
 			if '/dl' in links:
 				#print links
 				self.dl_list.append(links)
-		print self.dl_list
+		#print self.dl_list
 		for i in self.dl_list:
 			#print 'local url'
-			print re.split(self.series_name, i, flags=re.IGNORECASE)[0]+self.series_name+'/'
-		print localurl
-		return self.dl_list
+			localurl.append(re.split(self.series_name, i, flags=re.IGNORECASE)[0]+self.series_name+'/')
+		#print localurl
+		return localurl
 
- 	def Crawler(self,Parent_link):
+	def Crawler(self,Parent_link):
 		'''
 		method suppose to give sitemap from the patent link
 		:param Parent_link:
 		:return:Not decided yet
 		'''
-		print 'in crawler'
-		self.send_search_request(Parent_link)
-
+		child_urls = None
+		print 'in crawler parent link : [%s]'%Parent_link
+		try:
+			print 'sending req for ',Parent_link
+			self.send_search_request(Parent_link)
+			child_urls =  self.get_http(Parent_link)
+		except urllib2.HTTPError:
+			print 'Parent link not found "%s" '%Parent_link
+			return
+		finally:
+			self.sitemap[Parent_link] = child_urls
+		for url in child_urls:
+			self.Crawler(url)
 	def print_links(self,link_list):
 		'''
 		prints all the links
@@ -84,3 +102,17 @@ class google:
 		for link in link_list:
 			print link
 
+	def get_http(self,Parent_link):
+		'''get child links '''
+		httpurl = []
+		localurl = []
+		print 'get_http'
+		alla = self.soup.find_all('a')
+		# self.print_links(alla)
+		for linkattributes in alla:
+			httpurl.append(linkattributes.get('href'))
+		httpurl = httpurl[1:]
+		for index in range(len(httpurl)):
+			httpurl[index] = Parent_link+httpurl[index]
+		print httpurl
+		return httpurl
