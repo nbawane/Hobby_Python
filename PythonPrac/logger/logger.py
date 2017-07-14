d3967989e4e9ad74031e1686c7611e70959325eb
nitesh bawane <nitesh.bawane1@gmail.com>
2017-07-14 16:08:02 +0530

    Flatten the list added

---
+++ PythonPrac/logger/logger.py
@@ -0,0 +1,24 @@
+import logging
+from os import path,remove
+
+class log:
+	def __init__(self,log_file_name):
+		"""
+
+		:param log_file_name: this must be the path of log file
+		"""
+		self.Log_file = log_file_name
+		self.init_logger()
+
+	def init_logger(self):
+		print 'logger initiated'
+		if path.exists(self.Log_file):
+			remove(self.Log_file)
+		logging.basicConfig(format='%(asctime)s %(message)s',filename=self.Log_file,level=logging.DEBUG)
+
+	def Info(self,str):
+		logging.info(str)
+		print str
+
+
+
