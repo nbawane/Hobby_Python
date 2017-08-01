6f73e4f152fa529d80d47c4f389deb20d66d839d
nitesh bawane <nitesh.bawane1@gmail.com>
2017-08-01 18:23:12 +0530

    scriptnameextracter added

--- PythonPrac/Script_Parameter_logger.py
+++ PythonPrac/Script_Parameter_logger.py
@@ -16,18 +16,27 @@ class XlsOpt:
 
 class FileOpt:
 	def __init__(self):
-		self.logpath    = "C:\Results\CA11_CQVolParallelism_CallAll_20170725_141758_5052.log"
+		self.logpath    = "C:\Results\CA11_CQVolParallelism_CallAll_20170801_174508_8476.log"
 		self.fd         = open(self.logpath)
 		self.glob_dict  = {'Start Block Address':None, 'Priority':None, 'Direction':None, 'TaskID':None, 'NumBlocks':None}
 		self.excelopt   = XlsOpt()
 		self.rownum     = 0
 		self.write_header()
-
+		self.scriptnamecolumn = 0
+		self.iterationnumcol  = 1
+		self.taskidcol		  = 2
+		self.addrcol		  = 3
+		self.lengthcol		  = 4
+		self.directioncol	  = 5
+		self.prioritycol      = 6
+		self.scriptname       = None
 	def parser(self):
+		self.rownum += 1
 		for line in self.fd:
-			if 'ITERATION' in line:
-				self.rownum += 1
-				self.excelopt.sheet.write_merge(self.rownum,self.rownum,0,4,line)
+			if 'Started' in line:
+				self.scriptname = (line.rstrip()).split(' ')[-1]
+				self.excelopt.write_at_location(self.rownum,self.scriptnamecolumn,self.scriptname)
+
 			if ('CQTaskInformation' in line) or ('CQStartBlockAddr' in line):
 				taskInformation = line[line.rfind('[')+1:line.rfind(']')]
 				tasksplit = taskInformation.split(',')
@@ -49,24 +58,26 @@ class FileOpt:
 		self.excelopt.save_file()
 
 	def write_task_id(self, row ,task_id):
-		self.excelopt.write_at_location(row,0,int(task_id))
+		self.excelopt.write_at_location(row,self.taskidcol,int(task_id))
 
 	def write_addr(self, row, addr):
-		self.excelopt.write_at_location(row, 1, addr)
+		self.excelopt.write_at_location(row, self.addrcol, addr)
 
 	def write_length(self, row, length):
-		self.excelopt.write_at_location(row, 2, int(length))
+		self.excelopt.write_at_location(row, self.lengthcol, int(length))
 
 	def write_direction(self, row, direction):
-		self.excelopt.write_at_location(row, 3, int(direction))
+		self.excelopt.write_at_location(row, self.directioncol, int(direction))
 
 	def write_priority(self,row, priority):
-		self.excelopt.write_at_location(row, 4, int(priority))
+		self.excelopt.write_at_location(row, self.prioritycol, int(priority))
 
 	def write_header(self):
-		header = ['TaskID','Start Block Address','NumBlocks', 'Direction','Priority']
+		header = ['Script_Name','ITERATION','TaskID','Start Block Address','NumBlocks', 'Direction','Priority']
 		for column,key in enumerate(header):
 			self.excelopt.write_at_location(0,column,key)
+
+
 
 
 if __name__ == '__main__':
